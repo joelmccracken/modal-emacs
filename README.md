e2cfc207690e8f3d61b85ab9b8f04abdde19747f
Joel McCracken <mccracken.joel@gmail.com>
2013-03-07 23:34:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Modal Emacs #
+
+There are a bunch of frameworks that emulate Vim keybindings in
+Emacs. This is not one of them.
+
+I used Vim for years, and I really liked it. I switched to Emacs, but
+that is besides the point. However, Emacs and Vim are very different
+beasts, and the way that they operate is also very different. Thus, it
+makes sense to.
+
+personally, i have developed some RSI and this makes all of this more
+pressing. 
+
+# installation #
+
+Don't know yet. Add the .el file to your loadpath and require it. 
+
+clearly this should be a package. 
+
+Im guessing that what will need to go here is some kind of
+instructions on enabling/disabling on a per-mode basis. 
+
+# bugs, ideas, suggestions, etc #
+
+Please direct all inquiries to issues! 
---
+++ modal-emacs.el
@@ -0,0 +1,75 @@
+(defun modal-emacs-on () (interactive) (modal-emacs-mode 1))
+(defun modal-emacs-off () (interactive) (modal-emacs-mode -1))
+
+(defvar modal--normal-mode-map "The standard keymap that starts everything")
+(setq modal--normal-mode-map (make-sparse-keymap))
+
+
+
+(define-key modal--normal-mode-map "a" 'move-beginning-of-line)
+(define-key modal--normal-mode-map "e" 'move-end-of-line)
+(define-key modal--normal-mode-map "i" 'modal--insert-mode)
+(define-key modal--normal-mode-map "n" 'next-line)
+(define-key modal--normal-mode-map "p" 'previous-line)
+(define-key modal--normal-mode-map "f" 'forward-char)
+(define-key modal--normal-mode-map "b" 'backward-char)
+(define-key modal--normal-mode-map "d" 'delete-char)
+(define-key modal--normal-mode-map " " 'set-mark-command)
+(define-key modal--normal-mode-map "o" 'other-window)
+(define-key modal--normal-mode-map "/" 'isearch-forward-regexp)
+(define-key modal--normal-mode-map "?" 'isearch-backward-regexp)
+(define-key modal--normal-mode-map "k" 'paredit-forward)
+(define-key modal--normal-mode-map "j" 'paredit-backward)
+(define-key modal--normal-mode-map "u" 'universal-argument)
+(define-key modal--normal-mode-map "<" 'beginning-of-buffer)
+(define-key modal--normal-mode-map ">" 'end-of-buffer)
+(define-key modal--normal-mode-map "v" 'scroll-up-command)
+(define-key modal--normal-mode-map "V" 'scroll-down-command)
+
+
+(defvar modal--normal-mode-map "emulates the spirit fo vims insert mode, retores functionality")
+(setq modal--insert-mode-map (make-sparse-keymap))
+(define-key modal--insert-mode-map (kbd "M-ESC") 'modal--normal-mode)
+(define-key modal--insert-mode-map (kbd "C-z") 'modal--normal-mode)
+
+
+(defun modal--insert-mode ()
+  ""
+  (interactive)
+  (setq insert-mode t)
+  (modal--switch-active-keymap 'insert-mode modal--insert-mode-map)
+  nil)
+
+
+(defun modal--normal-mode ()
+  ""
+  (interactive)
+  (setq normal-mode t)
+  (modal--switch-active-keymap 'normal-mode modal--normal-mode-map))
+
+
+(defun modal--meta-mode())
+
+
+(defvar modal--active-keymap-alist nil
+  "contains the keymap that is currently active")
+
+(defun modal--switch-active-keymap (mode-name new-keymap)
+  ;; we only ever have  a single active keymap
+  (setq modal--active-keymap-alist 
+        `((,mode-name . ,new-keymap))))
+
+(define-minor-mode modal-emacs-mode
+  "minor mode that enables our semi-simplistic \"modal\" keymap system"
+  :lighter " Modal"
+  :group 'modal-emacs
+  (if (not modal-emacs-mode)
+      (setq emulation-mode-map-alists (delq 'modal--active-keymap-alist emulation-mode-map-alists))
+    (add-to-ordered-list 'emulation-mode-map-alists 'modal--active-keymap-alist 400)
+    (modal--insert-mode)))
+
+(define-globalized-minor-mode modal-emacs-globalized-mode modal-emacs-mode
+  modal-emacs-on)
+
+
+(provide 'modal-emacs)
